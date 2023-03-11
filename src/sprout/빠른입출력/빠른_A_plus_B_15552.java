8f7e1094da1fa17e19602133a2ba9c8d82fe3e4f
jiseong0612 <hth5545@gmail.com>
2023-03-12 01:19:13 +0900

    백준 새싹 빠른입출력 완료

---
+++ src/sprout/빠른입출력/빠른_A_plus_B_15552.java
@@ -0,0 +1,34 @@
+package sprout.빠른입출력;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+
+//백준에선 채점시 제출한 코드를 Main.java로 가정하여 받는다.
+//그렇기때문에 Main클래스를 이용해주어야 컴파일 오류가 발생하지 않는다.
+public class 빠른_A_plus_B_15552 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+ 
+ 
+		int N = Integer.parseInt(br.readLine());
+        
+		StringTokenizer st;
+ 
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()))+ "\n");
+		}
+		br.close();
+        
+		bw.flush();
+		bw.close();
+	}
+}
